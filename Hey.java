53d81efd7f568eaefb445b53319c6d72cf4dae80
amitpal603-ops <ap036776@gmail.com>
2025-08-04 19:15:59 +0530

    first program in java

---
+++ Hey.java
@@ -0,0 +1,13 @@
+public class Hey {
+
+    public static void main(String[] args){
+        
+        
+        System.out.println("First 10 odd number :");
+        for(int i=1; i<=20; i++){
+            if(i%2 != 0){
+        System.out.println(i);
+            }
+    }
+}
+}
